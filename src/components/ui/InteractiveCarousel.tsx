7eb46e7413a8b786bf27afed3478e9430a2a7bff
Jun Hao <yan46648@gmail.com>
2024-05-27 13:16:21 +0630

    feat: all done

--- src/components/ui/InteractiveCarousel.tsx
+++ src/components/ui/InteractiveCarousel.tsx
@@ -14,70 +14,70 @@ const InteractiveCarousel = () => {
       description: "Businessman",
       image: "/person1.svg",
       comment:
-        "Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.",
+        "Sinisana's services have significantly enhanced our food security measures. We trust their expertise completely.",
     },
     {
       name: "John Doe",
       description: "Entrepreneur",
       image: "/person2.svg",
       comment:
-        "Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.",
+        "The blockchain technology used by Sinisana ensures our products are sustainably sourced and ethically produced.",
     },
     {
       name: "Emily Smith",
       description: "CEO",
       image: "/person3.svg",
       comment:
-        "Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.",
+        "We have seen a marked improvement in our lifecycle tracking since partnering with Sinisana. Highly recommended.",
     },
     {
       name: "Michael Johnson",
       description: "Investor",
-      image: "/farm.svg",
+      image: "/person4.jpg",
       comment:
-        "Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.",
+        "Sinisana's halal compliance solutions are second to none. They have helped us maintain the highest standards.",
     },
     {
       name: "Samantha Lee",
       description: "Business Owner",
-      image: "/person2.svg",
+      image: "/person5.jpg",
       comment:
-        "Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.",
+        "Since we started using Sinisana, we've noticed a significant boost in our customer trust levels. Their innovative approach to supply chain transparency is a game changer.",
     },
     {
       name: "David Williams",
       description: "Manager",
-      image: "/person3.svg",
+      image: "/person6.jpg",
       comment:
-        "Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.",
+        "Sinisana's advanced data analytics have provided us with invaluable insights into our product lifecycle. Their technology is a must-have for any forward-thinking brand.",
     },
     {
       name: "Sophia Martinez",
       description: "Director",
-      image: "/person1.svg",
+      image: "/person7.jpg",
       comment:
-        "Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.",
+        "Working with Sinisana has revolutionized our supply chain management. Their ability to provide real-time data has streamlined our operations and improved efficiency.",
     },
     {
       name: "Matthew Brown",
       description: "Founder",
-      image: "/person2.svg",
+      image: "/person8.jpg",
       comment:
-        "Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.",
+        "The transparency Sinisana provides has been instrumental in building our brand's reputation for integrity. Consumers love knowing the full story behind our products.",
     },
     {
       name: "Olivia Taylor",
       description: "Consultant",
-      image: "/person3.svg",
+      image: "/person9.jpg",
       comment:
-        "Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.",
+        "Thanks to Sinisana, we can confidently assure our customers of our sustainability practices. Their blockchain solutions are unmatched in the industry.",
     },
     {
       name: "William Clark",
       description: "Executive",
-      image: "/person1.svg",
+      image: "/person10.jpg",
       comment:
-        "Nisl rhoncus mattis rhoncus urna neque viverra. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum.",
+        "Partnering with Sinisana has been a pivotal decision for our brand. Their expertise in ensuring product authenticity and ethical sourcing is truly exceptional.",
     },
   ];
 
@@ -95,12 +95,12 @@ const InteractiveCarousel = () => {
             className="md:basis-1/2 ipad-pro:basis-1/2 lg:basis-1/3 "
           >
             <div className="p-1 flex justify-center items-center h-full">
-              <Card className="lg:w-[370px] group md:w-[370px] w-full md:h-[419px] lg:h-[419px] items-center justify-center border-0 shadow-none border-t-8  bg-cardbg hover:scale-105 transform transition-transform duration-300 ">
+              <Card className="lg:w-[370px] group md:w-[370px] select-none w-full md:h-[419px] lg:h-[419px] items-center justify-center border-0 shadow-none border-t-8  bg-cardbg hover:scale-105 transform transition-transform duration-300 ">
                 <CardContent className="flex aspect-square items-center justify-center">
                   <div className="items-center flex flex-col justify-center ">
                     <div className="items-center flex flex-col justify-center pb-40 group-hover:pb-[159px] bg-white w-[370px] mt-[-24px]">
                       <img src="/semicolIcon.svg" className="mt-10" />
-                      <p className="mt-4 text-center text-xs lg:px-9 md:px-9 lg:text-base md:text-base px-20">
+                      <p className="mt-4 text-center text-xs lg:px-5 md:px-5 lg:text-base md:text-base px-14">
                         {item.comment}
                       </p>
                     </div>
@@ -112,7 +112,7 @@ const InteractiveCarousel = () => {
                     /> */}
                     <div className="w-[67.08px] h-[73px] overflow-hidden rounded-full mt-5">
                       <img
-                        src={`/images/${item.image}`}
+                        src={`/testimonial-profile/${item.image}`}
                         alt={item.name}
                         className="w-full h-full object-cover"
                       />
